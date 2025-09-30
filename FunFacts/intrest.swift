11a591a9aac635c23e09a8668237bbc3e0113948
gida <geea62896@gmail.com>
2025-09-30 15:58:04 +0300

    ghaida

---
+++ FunFacts/intrest.swift
@@ -0,0 +1,18 @@
+//
+//  intrest.swift
+//  FunFacts
+//
+//  Created by Ghida Abdullah al-Mughamer on 08/04/1447 AH.
+//
+
+import SwiftUI
+
+struct intrest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    intrest()
+}
