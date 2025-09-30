4a5751dde5104f6e3aa279320d13eaa4e0a16e09
gida <geea62896@gmail.com>
2025-09-30 15:55:53 +0300

    ghida

---
+++ FunFacts/signin.swift
@@ -0,0 +1,18 @@
+//
+//  signin.swift
+//  FunFacts
+//
+//  Created by Ghida Abdullah al-Mughamer on 08/04/1447 AH.
+//
+
+import SwiftUI
+
+struct signin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    signin()
+}
