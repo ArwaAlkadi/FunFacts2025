fb8b0ec29c7d7e67e363e4d0800e4d52bb7b0c85
Arwa <Arwaalkadi8@gmail.com>
2025-10-02 15:35:10 +0300

    edit

---
+++ FunFacts/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  FunFacts
+//
+//  Created by Arwa Alkadi on 01/10/2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
